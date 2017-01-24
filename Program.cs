86c746e99890d851971d35948709f94e04f404b2
Heogle <hsi6446@gmail.com>
2017-01-24 13:47:50 +0900

    01

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           string yourName;
+           Console.WriteLine("What is your name?");
+
+           yourName = Console.ReadLine();
+           Console.WriteLine("Hello. {0}", yourName);
+
+           Console.WriteLine("What is your age?");
+
+           int age = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("You are {0} years old.", age);
+           */
+
+            /*
+             올해는 뭐라도 하나 만들어보자. 힘들어도 해보자. 재미있게 해보자. 
+             나도 파이널판타지 시리즈 만들어 보고 싶다. 만들 수 있을 것이다. 만들 수 있다.
+             */
+
+            /*
+            var n1 = true;
+            n1 = false;
+            Console.WriteLine(n1);
+            */
+
+            /*
+             * abstsract 가능
+            int x = 10;
+            int y = 8;
+            Console.WriteLine(x - y);
+            */
+
+            /*
+             * 값이 다르게 출력될 수 있음. 괄호 인식함.
+            int x = 3 + 6 * 7;
+            Console.WriteLine(x);
+
+            int y = (3 + 6) * 7;
+            Console.WriteLine(y);
+            */
+
+            
+            /* x++로 그 다음 숫자 출력 가능. -
+            int x = 10;
+            x++;
+            Console.WriteLine(x);
+            */
+            
+
+            /*
+            //prefix
+            int x = 3;
+            int y = ++x;
+            Console.WriteLine("value {0}, {1}", x, y);
+
+            //postfix
+            int a = 3;
+            int b = a++;
+            Console.WriteLine("vlaue {0}, {1}", a, b);
+            */
+        }
+    }
+}
